aa3b72dd492e8b05163b45e845c7154ca6723822
SamuelePassiatore <samuw.978@gmail.com>
2024-12-02 17:57:29 +0100

    create components & temporary home page

---
+++ frontend/app/_components/AudioInput.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from 'react';
+
+const AudioInput = () => {
+  return (
+    <div>
+      <button>Start Recording</button>
+    </div>
+  );
+};
+
+export default AudioInput;
---
+++ frontend/app/_components/TextInput.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react';
+
+const TextInput = () => {
+  return (
+    <textarea placeholder="Type your message here..." rows={4} cols={50} />
+  );
+};
+
+export default TextInput;
