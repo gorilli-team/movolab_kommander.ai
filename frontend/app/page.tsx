d5ce2761c70c3930bf4db17b68c422d80c02dcb7
SamuelePassiatore <samuw.978@gmail.com>
2024-12-03 10:50:27 +0100

    home page graphic done

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import React, { useState } from 'react';
-import TextInput from './_components/TextInput';
-import AudioInput from './_components/AudioInput';
+import TextMessage from './_components/TextMessage';
+import AudioMessage from './_components/AudioMessage';
+import { Banner, Button, Label, TextInput } from "flowbite-react";
+import { HiX } from "react-icons/hi";
 
 export default function Home() {
 
@@ -14,14 +16,25 @@ export default function Home() {
 
   return (
     <div className='flex flex-col w-full h-screen items-center home-page'>
-      <h1>What would you like to do?</h1>
-      <div>
-        <button onClick={() => handleIconClick('text')}>🖊️</button>
-        <button onClick={() => handleIconClick('audio')}>🎙️</button>
-      </div>
-
-      {inputMethod === 'text' && <TextInput />}
-      {inputMethod === 'audio' && <AudioInput />}
+      <Banner>
+        <div className="banner-custom flex w-full items-center justify-between border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
+          <div className="flex w-full shrink-0 items-center sm:w-auto">
+            <form action="#" className="flex w-full flex-col items-center md:flex-row md:gap-x-3">
+              <h2 className='font-bold'>Cosa vuoi fare?</h2>
+            </form>
+          </div>
+        </div>
+      </Banner>
+      <Banner>
+        <div className="banner-custom-bottom flex w-full items-center justify-center border-b border-gray-200 bg-gray-50 p-4 dark:border-gray-600 dark:bg-gray-700">
+          <div className='flex justify-between'>
+            <div className='div-button mr-2'><Button onClick={() => handleIconClick('text')} color="light"><i className="fa-solid fa-keyboard pr-4"></i>Scrivi</Button></div>
+            <div className='div-button ml-2'><Button onClick={() => handleIconClick('audio')} color="light"><i className="fa-solid fa-microphone pr-4"></i>Parla</Button></div>            
+          </div>
+        </div>
+      </Banner>
+      {inputMethod === 'text' && <TextMessage />}
+      {inputMethod === 'audio' && <AudioMessage />}
     </div>
   );
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,13 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [require("flowbite/plugin")],
+}
+
