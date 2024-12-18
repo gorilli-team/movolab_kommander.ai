cfbb708a2c0328eaa4f271caa9a91005eb72464d
SamuelePassiatore <samuw.978@gmail.com>
2024-12-18 19:08:47 +0100

    refactoring components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,153 +1,180 @@
 "use client";
 
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import { Button, Spinner } from "flowbite-react";
-import TextMessage from "./_components/TextMessage";
-
-// Caricamento dinamico del componente AudioMessage (non server-side renderizzato)
-const AudioMessage = dynamic(() => import("./_components/AudioMessage"), {
-  ssr: false,
-});
+import React, { useState, useEffect, useRef } from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
 
 export default function Dashboard() {
-  const [inputMethod, setInputMethod] = useState<"text" | "audio" | null>(null);
   const [message, setMessage] = useState<string>("");
   const [audioFile, setAudioFile] = useState<Blob | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [requestStatus, setRequestStatus] = useState<string | null>(null);
+  const [isRecording, setIsRecording] = useState(false);
 
-  // Funzione per inviare un messaggio di testo
+  const [userMessages, setUserMessages] = useState<any[]>([]);
+  const [kommanderMessages, setKommanderMessages] = useState<any[]>([]);
+
+  const { startRecording, stopRecording, status, mediaBlobUrl } = useReactMediaRecorder({
+    audio: true,
+    onStop: (blobUrl, blob) => {
+      setAudioFile(blob);
+      setIsRecording(false);
+    },
+  });
+
   const handleTextSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!message) return;
+
     setIsLoading(true);
-    setRequestStatus(null);
 
-    try {
-      const response = await fetch("http://localhost:5000/new_message", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ message_text: message, message_type: "text" }),
-      });
+    setUserMessages((prevMessages) => [
+      ...prevMessages,
+      { type: "user", content: message, time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) },
+    ]);
 
-      if (response.ok) {
-        const result = await response.json();
-        setRequestStatus("success");
-      } else {
-        setRequestStatus("error");
-      }
-    } catch (error) {
-      setRequestStatus("error");
-    } finally {
-      setIsLoading(false);
-    }
+    setMessage("");
+
+    setTimeout(() => {
+      setKommanderMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "kommander", content: "Risposta automatica di Kommander", time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) },
+      ]);
+    }, 1000);
+
+    setIsLoading(false);
   };
 
-  // Funzione per inviare un messaggio audio
   const handleAudioSubmit = async () => {
     if (!audioFile) return;
+
     setIsLoading(true);
-    setRequestStatus(null);
 
-    try {
-      const formData = new FormData();
-      formData.append("audio", audioFile, "audioMessage.wav");
+    setUserMessages((prevMessages) => [
+      ...prevMessages,
+      { type: "user", content: audioFile, time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) },
+    ]);
 
-      const response = await fetch("http://localhost:5000/upload-audio", {
-        method: "POST",
-        body: formData,
-      });
+    setAudioFile(null);
 
-      if (response.ok) {
-        const result = await response.json();
-        setRequestStatus("success");
-      } else {
-        setRequestStatus("error");
-      }
-    } catch (error) {
-      setRequestStatus("error");
-    } finally {
-      setIsLoading(false);
+    setTimeout(() => {
+      setKommanderMessages((prevMessages) => [
+        ...prevMessages,
+        { type: "kommander", content: "Risposta automatica di Kommander", time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) },
+      ]);
+    }, 1000);
+
+    setIsLoading(false);
+  };
+
+  const handleMicrophoneClick = () => {
+    if (isRecording) {
+      stopRecording();
+      setIsRecording(false);
+    } else {
+      startRecording();
+      setIsRecording(true);
     }
   };
 
   return (
-    <div className="flex flex-col w-full h-screen items-center page-custom">
-      <div className="widget-custom rounded-lg shadow-lg bg-white w-full max-w-md">
-
-        {/* Titolo */}
+    <div className="overflow-auto flex w-full h-screen justify-center items-center bg-gray-100">
+      <div className="widget-custom rounded-lg shadow-lg bg-white">
         <div className="banner-custom-title flex items-center border-b border-gray-200 bg-gray-50 p-4">
           <h2 className="font-bold">Cosa vuoi fare oggi?</h2>
         </div>
 
-        {/* Componente per messaggi di testo */}
-        {inputMethod === "text" && (
-          <div className="banner-custom-chat p-4 space-y-6">
-            <TextMessage
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onSubmit={handleTextSubmit}
-            />
-          </div>
-        )}
-
-        {/* Componente per messaggi audio */}
-        {inputMethod === "audio" && (
-          <div className="banner-custom-chat p-4 space-y-6">
-            <AudioMessage
-              onAudioReady={(blob) => setAudioFile(blob)} // Imposta il file audio quando è pronto
-            />
-            {/* Se il file audio è pronto, aggiungi un pulsante per inviarlo */}
-            {audioFile && (
-              <div className="mt-4 flex justify-center">
-                <Button onClick={handleAudioSubmit} color="gray" disabled={isLoading}>
-                  Invia Audio
-                </Button>
+        <div className="banner-custom-chat p-4">
+          {userMessages.map((message, index) => (
+            <div key={index} className="w-full">
+              <div className="banner-chat-user flex">
+                <img
+                  className="logo-kommander-chat"
+                  src={message.type === "user" ? "./spiaggia-tramonto.png" : "./kommander-logo.png"}
+                  alt={`${message.type}-icon`}
+                />
+                <div>
+                  <div className="info-message-div">
+                    <span className="font-bold type-user">{message.type === "user" ? "User" : "Kommander.ai"}</span>
+                    <span className="hour-message">{message.time}</span>
+                  </div>
+                  {message.type === "user" && typeof message.content === "string" ? (
+                    <p className="message pt-1">{message.content}</p>
+                  ) : message.type === "user" && message.content instanceof Blob ? (
+                    <div className="w-full max-w-md mb-2 ml-2">
+                      <audio controls src={URL.createObjectURL(message.content)} />
+                    </div>
+                  ) : (
+                    <p className="message pt-1">{message.content}</p>
+                  )}
+                </div>
               </div>
-            )}
-          </div>
-        )}
+            </div>
+          ))}
 
-        {/* Stato di caricamento */}
-        {isLoading && <Spinner />}
+          {kommanderMessages.map((message, index) => (
+            <div key={index} className="w-full flex justify-end">
+              <div className="banner-chat-kommander flex">
+                <img
+                  className="logo-kommander-chat"
+                  src="./kommander-logo.png"
+                  alt="kommander-icon"
+                />
+                <div>
+                  <div className="info-message-div">
+                    <span className="font-bold type-user">Kommander.ai</span>
+                    <span className="hour-message">{message.time}</span>
+                  </div>
+                  <p className="message pt-1">{message.content}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
 
-        {/* Messaggio di stato della richiesta */}
-        {requestStatus && (
-          <div>
-            {requestStatus === "success" ? "Richiesta completata con successo!" : "Errore nella richiesta."}
-          </div>
-        )}
-
-        {/* Footer per l'input dell'utente */}
         <div className="banner-custom-footer flex w-full items-center justify-between border-t border-gray-200 bg-gray-50 p-4">
-          <div className="flex w-full items-center rounded-md border border-gray-300 p-2 bg-white mb-2">
-            <input
-              className="w-full outline-none"
+          <div className="flex w-full items-center text-area-div rounded-md border border-gray-300 p-2 bg-white">
+            <textarea
+              className="w-full text-area-custom outline-none resize-none"
               value={message}
               onChange={(e) => setMessage(e.target.value)}
               placeholder="Scrivi un messaggio..."
-              disabled={inputMethod === "audio"} // Disabilita l'input di testo durante la registrazione audio
+              disabled={isRecording}
             />
             <button
               className="btn-microphone ml-2 text-gray-600 hover:text-blue-500 focus:outline-none"
-              onClick={() => setInputMethod(inputMethod === "audio" ? null : "audio")}
+              onClick={handleMicrophoneClick}
             >
-              <i className="fa-solid fa-microphone text-xl"></i>
+              {isRecording ? (
+                <i className="fa-regular fa-circle-stop text-xl"></i>
+              ) : (
+                <i className="fa-solid fa-microphone text-xl"></i>
+              )}
             </button>
           </div>
 
-          <div className="flex banner-buttons">
+          <div className="flex flex-col buttons-div items-center w-full space-y-2">
             <button
-              className="btn-send"
-              onClick={inputMethod === "audio" ? handleAudioSubmit : handleTextSubmit}
-              disabled={inputMethod === "audio" ? !audioFile : !message}
+              className="btn-send py-2 px-4 text-sm"
+              onClick={handleTextSubmit}
+              disabled={isRecording || !message}
             >
-              {inputMethod === "audio" ? "Invia Audio" : "Invia Messaggio"}
+              <span><i className="fa-solid fa-envelope"></i></span>
+            </button>
+
+            <button
+              className="btn-send py-2 px-4 text-sm"
+              onClick={handleAudioSubmit}
+              disabled={isRecording || !audioFile}
+            >
+              <span><i className="fa-solid fa-file-audio"></i></span>
             </button>
           </div>
         </div>
 
+        {mediaBlobUrl && (
+          <div className="w-full max-w-md mb-2 ml-2">
+            <audio controls src={mediaBlobUrl} />
+          </div>
+        )}
       </div>
     </div>
   );
