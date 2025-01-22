ee41e639dbc9ea280f7578bdd1333a89063f8100
SamuelePassiatore <samuw.978@gmail.com>
2025-01-22 20:29:03 +0100

    updated chat gpt controller

--- backend/src/controllers/chatGptController.ts
+++ backend/src/controllers/chatGptController.ts
@@ -61,11 +61,11 @@ export const callChatGpt = async (text: string, texts: string[]): Promise<Record
   4. Il nome del cliente.
   5. Il numero di telefono del conducente.
   6. Il numero di telefono del cliente.
-  7. Il gruppo di veicoli (id, mnemonic, description). Tendenzialmente il gruppo è rappresentato dal tipo di veicolo. Puoi anche scegliere tutti i gruppi quindi devi prenderli tutti. Se dice mostrameli tutti, prendili tutti. Per esempio, se ricevi l'indicazione "voglio noleggiare una moto" devi scegliere solo il gruppo moto.
-  8. Il workflow (id, nome). Attenzione tra prepagato prenotazione e prepagato apertura movo o anche solo tarvisio. Deve essere esplicita l'informazione.
+  7. Il gruppo di veicoli (id, mnemonic, description). Tendenzialmente il gruppo è rappresentato dal tipo di veicolo. Puoi anche scegliere tutti i gruppi quindi devi prenderli tutti. Se dice mostrameli tutti, prendili tutti.
+  8. Il workflow (id, nome). Attenzione tra prepagato prenotazione e prepagato apertura movo o anche solo tarvisio. Deve essere esplicita l'informazione. (es. prepagato prenotazione)
   9. PickUpLocation (id, nome), è legato a rental location.
   10. DropOffLocation (id, nome), è legato a rental location.
-  11. Il tipo di movimento, sicuramente ti verrà indicato il nome e non l'enum. 
+  11. Il tipo di movimento, sicuramente ti verrà indicato il nome e non l'enum (es. noleggio)
   12. Response (response text, missing parameters). 
     =>  - ResponseText: Un messaggio indicativo riguardo l'esito della richiesta. Se ci sono tutti i parametri scrivi "Richiesta riuscita!". Se manca anche solo un parametro devi scrivere una frase del genere "Per completare la tua richiesta ho bisogno delle seguenti informazioni: parametro1, parametro2, etc.".
         - MissingParameters: Un array dove vengo inseriti i parametri mancanti, se non mancano parametri allora l'array sarà vuoto.
